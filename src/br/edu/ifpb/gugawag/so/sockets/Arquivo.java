6b72bb19eb9d9a1287f37ce7f8198ae629060ec5
Erick Fonseca <erickboyfla@gmail.com>
2024-03-19 23:29:41 -0300

    create arquivo

---
+++ src/br/edu/ifpb/gugawag/so/sockets/Arquivo.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.gugawag.so.sockets;
+
+public class Arquivo {
+    private String nome;
+
+    public Arquivo(String nome) {
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
